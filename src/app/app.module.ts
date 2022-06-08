dc52a278fbf1a61598d156e07f3ab59183aea36f
Sir.Onnti <nicolasbaseggio1@gmail.com>
2022-06-08 03:58:23 -0300

    Header body y bienvenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BodyComponent } from './component/body/body.component';
+import { BienvenidoComponent } from './component/bienvenido/bienvenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    BienvenidoComponent
   ],
   imports: [
     BrowserModule
